cb849ea15bf1cb89fa33962bf207dc4b87e7151b
takpika <takpika@mail.takpika.jp>
2023-12-17 21:02:06 +0800

    fix issue

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ logger = logging.getLogger("main.py")
 
 token = os.environ.get("DISCORD_TOKEN")
 userID = os.environ.get("DISCORD_USER_ID")
+openAIToken = os.environ.get("OPENAI_API_KEY")
 if token is None or token == "":
     logger.error("Please Set DISCORD_TOKEN")
     exit(1)
-if os.environ.get("OPENAI_API_KEY") is None or os.environ.get("OPENAI_API_KEY") == "":
+if openAIToken is None or openAIToken == "":
     logger.error("Please Set OPENAI_API_KEY")
     exit(1)
 if userID is None or userID == "":
